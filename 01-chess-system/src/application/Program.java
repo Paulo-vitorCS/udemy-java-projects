b045302bae44faad8f73786008a11f84f35df7a0
Paulo Vitor Costa Silva <65197091+Paulo-vitorCS@users.noreply.github.com>
2022-09-23 14:54:53 -0300

    Chess layer ant printing the board

--- 01-chess-system/src/application/Program.java
+++ 01-chess-system/src/application/Program.java
@@ -1,13 +1,15 @@
 package application;
 
-import boardgame.Position;
+import boardgame.*;
+import chess.*;
 
 public class Program {
 
     public static void main(String[] args) {
         
-        Position pos = new Position(3,5);
-        System.out.println(pos);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+        //System.out.println(pos);
         
     }
     
--- 01-chess-system/src/boardgame/Position.java
+++ 01-chess-system/src/boardgame/Position.java
@@ -4,10 +4,6 @@ public class Position {
     
     private int row, column;
     
-    public Position() {
-        
-    }
-    
     public Position(int row, int column) {
         this.row = row;
         this.column = column;
