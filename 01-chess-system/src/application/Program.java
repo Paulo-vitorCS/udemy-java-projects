82dd557105e11f511d86c1bb76ca50d1b7b53bd7
Paulo Vitor Costa Silva <65197091+Paulo-vitorCS@users.noreply.github.com>
2022-10-06 13:27:57 -0300

    Moving Pieces

--- 01-chess-system/src/application/Program.java
+++ 01-chess-system/src/application/Program.java
@@ -2,14 +2,29 @@ package application;
 
 import boardgame.*;
 import chess.*;
+import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
         
+        Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
-        //System.out.println(pos);
+        
+        while (true) {
+            UI.printBoard(chessMatch.getPieces()); 
+            System.out.println();
+            System.out.println("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+           
+           
+            System.out.println();
+            System.out.println("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+            
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            
+        }       
         
     }
     
