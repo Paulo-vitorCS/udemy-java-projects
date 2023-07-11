eaa36012a347e237f7be6be84da64f82edf0b997
Paulo Vitor Costa Silva <65197091+Paulo-vitorCS@users.noreply.github.com>
2023-07-11 16:29:40 -0300

    update and delete implementation

--- 02-demo-dao-jdbc/src/model/dao/impl/SellerDaoJDBC.java
+++ 02-demo-dao-jdbc/src/model/dao/impl/SellerDaoJDBC.java
@@ -59,12 +59,46 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void update(Seller seller) {
+        PreparedStatement st = null;
 
+        try {
+            st = conn.prepareStatement(
+                    "update seller " +
+                            "set Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? " +
+                            "where id = ?",
+                    Statement.RETURN_GENERATED_KEYS
+            );
+
+            st.setString(1, seller.getName());
+            st.setString(2, seller.getEmail());
+            st.setDate(3, new java.sql.Date(seller.getBirthDate().getTime()));
+            st.setDouble(4, seller.getBaseSalary());
+            st.setInt(5, seller.getDepartment().getId());
+
+            st.setInt(6, seller.getId());
+
+            st.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DBException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
     public void deleteById(Integer id) {
+        PreparedStatement st = null;
 
+        try {
+            st = conn.prepareStatement("delete from seller where id = ?");
+            st.setInt(1, id);
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new DBException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
