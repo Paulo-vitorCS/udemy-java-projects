3a81e0cb0fb94794fa220d88328d17c2e20ec3b0
Paulo Vitor Costa Silva <65197091+Paulo-vitorCS@users.noreply.github.com>
2023-07-26 20:57:15 -0300

    OrderStatus enum

--- 03-workshop-springboot3-jpa/src/main/java/br/com/course/config/TestConfig.java
+++ 03-workshop-springboot3-jpa/src/main/java/br/com/course/config/TestConfig.java
@@ -2,6 +2,7 @@ package br.com.course.config;
 
 import br.com.course.entities.Order;
 import br.com.course.entities.User;
+import br.com.course.entities.enums.OrderStatus;
 import br.com.course.repositories.OrderRepository;
 import br.com.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +28,9 @@ public class TestConfig implements CommandLineRunner {
         User user1 = new User(null, "Maria Brown", "maria@gmail.com", "988776655", "123456");
         User user2 = new User(null, "Alex Green", "alex@gmail.com", "999775533", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2023-06-20T19:53:07Z"), user1);
-        Order o2 = new Order(null, Instant.parse("2023-07-21T03:42:10Z"), user2);
-        Order o3 = new Order(null, Instant.parse("2023-07-22T15:21:22Z"), user1);
+        Order o1 = new Order(null, Instant.parse("2023-06-20T19:53:07Z"), OrderStatus.PAID, user1);
+        Order o2 = new Order(null, Instant.parse("2023-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, user2);
+        Order o3 = new Order(null, Instant.parse("2023-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, user1);
 
         userRepository.saveAll(Arrays.asList(user1, user2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
