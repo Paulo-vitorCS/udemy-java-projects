73bf557cc725f6f35c82b9273f161db571e91225
Paulo Vitor Costa Silva <65197091+Paulo-vitorCS@users.noreply.github.com>
2023-07-26 16:25:06 -0300

    JPA repository, dependency injection, database seeding

---
+++ 03-workshop-springboot3-jpa/src/main/java/br/com/course/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package br.com.course.repositories;
+
+import br.com.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface  UserRepository extends JpaRepository<User, Long> {
+
+
+
+}
