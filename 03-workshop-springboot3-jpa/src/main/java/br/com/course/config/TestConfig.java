c0cc079872c157dad0c1364e76a77a32f9bb98df
Paulo Vitor Costa Silva <65197091+Paulo-vitorCS@users.noreply.github.com>
2023-07-27 11:13:28 -0300

    Category

--- 03-workshop-springboot3-jpa/src/main/java/br/com/course/config/TestConfig.java
+++ 03-workshop-springboot3-jpa/src/main/java/br/com/course/config/TestConfig.java
@@ -1,8 +1,10 @@
 package br.com.course.config;
 
+import br.com.course.entities.Category;
 import br.com.course.entities.Order;
 import br.com.course.entities.User;
 import br.com.course.entities.enums.OrderStatus;
+import br.com.course.repositories.CategoryRepository;
 import br.com.course.repositories.OrderRepository;
 import br.com.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
         User user1 = new User(null, "Maria Brown", "maria@gmail.com", "988776655", "123456");
@@ -32,7 +37,12 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2023-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, user2);
         Order o3 = new Order(null, Instant.parse("2023-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, user1);
 
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
         userRepository.saveAll(Arrays.asList(user1, user2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+        categoryRepository.saveAll(Arrays.asList(cat1,cat2, cat3));
     }
 }
---
+++ 03-workshop-springboot3-jpa/src/main/java/br/com/course/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package br.com.course.repositories;
+
+import br.com.course.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+
+
+}
