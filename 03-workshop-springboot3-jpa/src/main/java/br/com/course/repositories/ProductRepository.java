61860b8edc17d423a839b135e08b9c21c6399f76
Paulo Vitor Costa Silva <65197091+Paulo-vitorCS@users.noreply.github.com>
2023-07-27 11:33:31 -0300

    Product

---
+++ 03-workshop-springboot3-jpa/src/main/java/br/com/course/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package br.com.course.repositories;
+
+import br.com.course.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+
+}
