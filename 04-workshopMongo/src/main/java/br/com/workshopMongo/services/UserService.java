dd66a4563bbdde36022e61a0f7f23c3a450921ab
Paulo Vitor Costa Silva <65197091+Paulo-vitorCS@users.noreply.github.com>
2023-07-29 18:49:01 -0300

    Insert User

--- 04-workshopMongo/src/main/java/br/com/workshopMongo/services/UserService.java
+++ 04-workshopMongo/src/main/java/br/com/workshopMongo/services/UserService.java
@@ -1,6 +1,7 @@
 package br.com.workshopMongo.services;
 
 import br.com.workshopMongo.domain.User;
+import br.com.workshopMongo.dto.UserDTO;
 import br.com.workshopMongo.repositories.UserRepository;
 import br.com.workshopMongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,14 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow( () -> new ObjectNotFoundException("Could not find object."));
+    }
+
+    public User insert(User obj) {
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO userDTO) {
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
 
 }
