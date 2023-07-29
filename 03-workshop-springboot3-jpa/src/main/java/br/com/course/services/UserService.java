0ae55e8d94fdec454e0759f48721ea970c70f38b
Paulo Vitor Costa Silva <65197091+Paulo-vitorCS@users.noreply.github.com>
2023-07-29 10:26:18 -0300

    Exception handling - update

--- 03-workshop-springboot3-jpa/src/main/java/br/com/course/services/UserService.java
+++ 03-workshop-springboot3-jpa/src/main/java/br/com/course/services/UserService.java
@@ -4,6 +4,7 @@ import br.com.course.entities.User;
 import br.com.course.repositories.UserRepository;
 import br.com.course.services.exceptions.DatabaseException;
 import br.com.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -43,9 +44,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
