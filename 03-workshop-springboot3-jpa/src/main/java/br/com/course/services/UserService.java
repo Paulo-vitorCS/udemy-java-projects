d708560e9d44e54562756f98689f4d55b26f0b3a
Paulo Vitor Costa Silva <65197091+Paulo-vitorCS@users.noreply.github.com>
2023-07-29 10:22:11 -0300

    Exception handling - delete

--- 03-workshop-springboot3-jpa/src/main/java/br/com/course/services/UserService.java
+++ 03-workshop-springboot3-jpa/src/main/java/br/com/course/services/UserService.java
@@ -2,8 +2,10 @@ package br.com.course.services;
 
 import br.com.course.entities.User;
 import br.com.course.repositories.UserRepository;
+import br.com.course.services.exceptions.DatabaseException;
 import br.com.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +31,15 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            if (repository.existsById(id)) {
+                repository.deleteById(id);
+            } else {
+                throw new ResourceNotFoundException(id);
+            }
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
