d8328ffb0bd3844ce4eaacac0d521e3c085eb25d
Paulo Vitor Costa Silva <65197091+Paulo-vitorCS@users.noreply.github.com>
2023-07-31 21:05:41 -0300

    Author data using DTO

--- 04-workshopMongo/src/main/java/br/com/workshopMongo/config/Intantiation.java
+++ 04-workshopMongo/src/main/java/br/com/workshopMongo/config/Intantiation.java
@@ -2,6 +2,7 @@ package br.com.workshopMongo.config;
 
 import br.com.workshopMongo.domain.Post;
 import br.com.workshopMongo.domain.User;
+import br.com.workshopMongo.dto.AuthorDTO;
 import br.com.workshopMongo.repositories.PostRepository;
 import br.com.workshopMongo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,10 +34,11 @@ public class Intantiation implements CommandLineRunner {
         User u2 = new User(null, "Alex Green", "alex@gmail.com");
         User u3 = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post p1 = new Post(null, sdf.parse("21/03/2023"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", u1);
-        Post p2 = new Post(null, sdf.parse("23/03/2023"), "Bom dia", "Acordei feliz hoje!", u1);
-
         userRepository.saveAll(Arrays.asList(u1, u2, u3));
+
+        Post p1 = new Post(null, sdf.parse("21/03/2023"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(u1));
+        Post p2 = new Post(null, sdf.parse("23/03/2023"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(u1));
+
         postRepository.saveAll(Arrays.asList(p1, p2));
     }
 
